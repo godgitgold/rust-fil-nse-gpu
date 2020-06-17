0557ff7fc15a42e13249822d0a2c5b387504b184
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2020-06-18 01:07:59 +0430

    feat: func for getting devices

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,5 @@
-use crate::GPUResult;
-use ocl::Device;
+use crate::{GPUError, GPUResult};
+use ocl::{Device, Platform};
 
 pub fn get_bus_id(d: Device) -> GPUResult<u32> {
     const CL_DEVICE_PCI_BUS_ID_NV: u32 = 0x4008;
@@ -8,4 +8,17 @@ pub fn get_bus_id(d: Device) -> GPUResult<u32> {
         + ((result[1] as u32) << 8)
         + ((result[2] as u32) << 16)
         + ((result[3] as u32) << 24))
+}
+
+pub const GPU_NVIDIA_PLATFORM_NAME: &str = "NVIDIA CUDA";
+
+pub fn get_devices(platform_name: &str) -> GPUResult<Vec<Device>> {
+    let platform = Platform::list()?.into_iter().find(|&p| match p.name() {
+        Ok(p) => p == platform_name,
+        Err(_) => false,
+    });
+    match platform {
+        Some(p) => Ok(Device::list_all(p)?),
+        None => Err(GPUError::Simple("GPU platform not found!")),
+    }
 }
